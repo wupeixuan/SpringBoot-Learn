dcf86ce97c68eb886b4bf52d0b5549407c97da20
wupeixuan <18603203910@163.com>
2020-05-16 22:15:04 +0800

    调整目录结构

--- database-version-control/src/main/java/com/wupx/DatabaseVersionControlApplication.java
+++ database-version-control/src/main/java/com/wupx/DatabaseVersionControlApplication.java
@@ -1,4 +1,4 @@
-package com.wupx.databaseversioncontrol;
+package com.wupx;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
