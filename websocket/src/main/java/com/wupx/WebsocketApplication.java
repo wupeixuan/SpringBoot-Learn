1efe09236e13857795602e3e7741442128ae155d
wupeixuan <18603203910@163.com>
2020-07-01 00:12:21 +0800

    Spring Boot 集成 WebSocket 实现服务端推送消息到客户端

---
+++ websocket/src/main/java/com/wupx/WebsocketApplication.java
@@ -0,0 +1,16 @@
+package com.wupx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wupx
+ */
+@SpringBootApplication
+public class WebsocketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebsocketApplication.class, args);
+    }
+
+}
---
+++ websocket/src/main/java/com/wupx/config/WebSocketConfiguration.java
@@ -0,0 +1,22 @@
+package com.wupx.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+
+/**
+ * WebSocket 配置
+ *
+ * @author wupx
+ * @date 2020/06/02
+ */
+@Configuration
+@EnableWebSocket
+public class WebSocketConfiguration {
+
+    @Bean
+    public ServerEndpointExporter serverEndpointExporter() {
+        return new ServerEndpointExporter();
+    }
+}
