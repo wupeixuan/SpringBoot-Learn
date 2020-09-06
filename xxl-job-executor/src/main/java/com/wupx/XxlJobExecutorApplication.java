f1e8dfea7b54f77235bd4f74ddab5eea1e60a926
wupeixuan <18603203910@163.com>
2020-09-06 23:32:30 +0800

    xxl-job集成

---
+++ xxl-job-executor/src/main/java/com/wupx/XxlJobExecutorApplication.java
@@ -0,0 +1,13 @@
+package com.wupx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XxlJobExecutorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XxlJobExecutorApplication.class, args);
+    }
+
+}
